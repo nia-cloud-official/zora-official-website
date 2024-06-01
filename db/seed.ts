1710f94724a5869c758418084b2fe29e0249ed74
nia-cloud-official <niacloudofficial@gmail.com>
2024-06-01 12:27:08 +0200

    modified:   db/config.ts 	modified:   db/seed.ts 	modified:   src/pages/api/waitlist.ts 	modified:   src/pages/newsletter.astro

--- db/seed.ts
+++
@@ -1,9 +0,0 @@
-import { db, WaitingList } from "astro:db";
-
-// https://astro.build/db/seed
-export default async function seed() {
-  await db.insert(WaitingList).values([
-    { email: "johndoe@example.com" },
-    { email: "janedoe@example.com" },
-  ]);
-}
--- src/pages/api/waitlist.ts
+++ src/pages/api/waitlist.ts
@@ -1,7 +1,6 @@
 export const prerender = false;
 
 import type { APIRoute } from "astro";
-import { WaitingList, db, sql } from "astro:db";
 
 export const GET: APIRoute = async () => {
   const waiting = await db
